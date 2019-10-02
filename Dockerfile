e94fd3a2c3427c00b0cada9691eb99dc87ea2d07
Jakub Hrozek <jhrozek@redhat.com>
2019-10-02 14:20:33 +0200

    Use a shell wrapper as an entry point

--- Dockerfile
+++ Dockerfile
@@ -14,6 +14,6 @@ RUN true \
     && yum clean all \
     && true
 
-#                  --verbose DEVEL
-ENTRYPOINT ["ocap-chroot", "/host", "xccdf", "eval", "--fetch-remote-resources"]
-CMD ["--profile", "xccdf_org.ssgproject.content_profile_ospp", "--rule", "xccdf_org.ssgproject.content_rule_no_empty_passwords", "--report", "/tmp/report.html", "/var/lib/content/ssg-rhel8-ds.xml"]
+COPY ./openscap-container-entrypoint.sh /
+RUN ["chmod", "+x", "/openscap-container-entrypoint.sh"]
+ENTRYPOINT ["/openscap-container-entrypoint.sh"]
---
+++ openscap-container-entrypoint.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+if [ -z $HOSTROOT ]; then
+    echo "hostroot not set"
+    exit 1
+fi
+
+if [ -z $PROFILE ]; then
+    echo "profile not set"
+    exit 1
+fi
+
+if [ -z $CONTENT ]; then
+    echo "content not set"
+    exit 1
+fi
+
+if [ -z $REPORT_DIR ]; then
+    echo "report_dit not set"
+    exit 1
+fi
+
+cmd=(
+    oscap-chroot $HOSTROOT xccdf eval \
+    --fetch-remote-resources \
+    --profile $PROFILE
+    )
+
+if [ ! -z $RULE ]; then
+    cmd+=(--rule $RULE)
+fi
+
+cmd+=(--report $REPORT)
+cmd+=($CONTENT)
+
+echo "Running oscap-chroot"
+echo "${cmd[@]}"
+"${cmd[@]}"
